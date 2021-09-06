9388c16c10490129e9d01ed8090b695a2d6b2d78
Solleti Harshitha <solletiharshitha@gmail.com>
2021-09-06 21:46:49 +0530

    [Harshitha] Add: Create Label tabel

---
+++ Models/LabelModel.cs
@@ -0,0 +1,55 @@
+﻿// ----------------------------------------------------------------------------------------------------------
+// <copyright file="LabelModel.cs" company="Bridgelabz"> 
+// Copyright © 2021 Company="BridgeLabz" 
+// </copyright> 
+// <creator name="Harshitha Solleti"/> 
+// ----------------------------------------------------------------------------------------------------------
+
+namespace Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Text;
+
+    /// <summary>
+    /// LabelModel class
+    /// </summary>
+    public class LabelModel
+    {
+        /// <summary>
+        /// Gets or sets label id
+        /// </summary>
+        [Key]
+        public int LabelId { get; set; }
+
+        /// <summary>
+        /// Gets or sets label name
+        /// </summary>
+        [Required]
+        public string LabelName { get; set; }
+
+        /// <summary>
+        /// Gets or sets note id
+        /// </summary>
+        [ForeignKey("NotesModel")]
+        public int NoteId { get; set; }
+
+        /// <summary>
+        /// Gets or sets notes model
+        /// </summary>
+        public virtual NotesModel NotesModel { get; set; }
+
+        /// <summary>
+        /// Gets or sets user id
+        /// </summary>
+        [ForeignKey("RegisterModel")]
+        public int? UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets register model
+        /// </summary>
+        public virtual RegisterModel RegisterModel { get; set; }
+    }
+}
---
+++ Repository/Migrations/20210906160606_Label.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class Label : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Label",
+                columns: table => new
+                {
+                    LabelId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LabelName = table.Column<string>(nullable: false),
+                    NoteId = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Label", x => x.LabelId);
+                    table.ForeignKey(
+                        name: "FK_Label_Notes_NoteId",
+                        column: x => x.NoteId,
+                        principalTable: "Notes",
+                        principalColumn: "NoteId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Label_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Label_NoteId",
+                table: "Label",
+                column: "NoteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Label_UserId",
+                table: "Label",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Label");
+        }
+    }
+}
