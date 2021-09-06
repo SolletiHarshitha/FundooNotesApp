2e4f26ff30a4f2550db24feacd277e4c55e8ada6
Solleti Harshitha <solletiharshitha@gmail.com>
2021-09-06 17:35:36 +0530

    [Harshitha] Add: AddCollaborator API

---
+++ Manager/Manager/CollaboratorManager.cs
@@ -0,0 +1,43 @@
+﻿// ----------------------------------------------------------------------------------------------------------
+// <copyright file="CollaboratorManager.cs" company="Bridgelabz"> 
+// Copyright © 2021 Company="BridgeLabz" 
+// </copyright> 
+// <creator name="Harshitha Solleti"/> 
+// ----------------------------------------------------------------------------------------------------------
+
+namespace Manager.Manager
+{
+    using global::Manager.Interface;
+    using Models;
+    using Repository.Interface;
+
+    /// <summary>
+    /// CollaboratorManager class
+    /// </summary>
+    public class CollaboratorManager : ICollaboratorManager
+    {
+        /// <summary>
+        /// Object for ICollaborator Repository
+        /// </summary>
+        private readonly ICollaboratorRepository repository;
+
+        /// <summary>
+        /// Initializes a new instance of the CollaboratorManager class
+        /// </summary>
+        /// <param name="repository">The Parameter</param>
+        public CollaboratorManager(ICollaboratorRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        /// <summary>
+        /// Add Collaborator Method
+        /// </summary>
+        /// <param name="collaborator">The Parameter</param>
+        /// <returns>Result of the boolean method</returns>
+        public bool AddCollaborator(CollaboratorModel collaborator)
+        {
+            return this.repository.AddCollaborator(collaborator);
+        }
+    }
+}
---
+++ Repository/Interface/ICollaboratorRepository.cs
@@ -0,0 +1,24 @@
+﻿// ----------------------------------------------------------------------------------------------------------
+// <copyright file="ICollaboratorRepository.cs" company="Bridgelabz"> 
+// Copyright © 2021 Company="BridgeLabz" 
+// </copyright> 
+// <creator name="Harshitha Solleti"/> 
+// ----------------------------------------------------------------------------------------------------------
+
+namespace Repository.Interface
+{
+    using Models;
+
+    /// <summary>
+    /// ICollaborator Repository Interface
+    /// </summary>
+    public interface ICollaboratorRepository
+    {
+        /// <summary>
+        /// AddCollaborator Method
+        /// </summary>
+        /// <param name="collaborator">The Parameter</param>
+        /// <returns>Result of Boolean Method</returns>
+        bool AddCollaborator(CollaboratorModel collaborator);
+    }
+}
